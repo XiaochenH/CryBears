805f20a64b220c10b43c8d41c5b850fbbf1bbcfb
韩笑尘 <hanx2021@berkeley.edu>
2018-11-14 19:37:03 -0800

    initial commit

---
+++ CryBears/CryBears/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryBears
+//
+//  Created by 韩笑尘 on 11/14/18.
+//  Copyright © 2018 韩笑尘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
