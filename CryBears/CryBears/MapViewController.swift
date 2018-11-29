49c70bb704d47a3bb8dddfd6fd856a90cc1e5491
Yunfang Xiao <emily@airbears2-10-142-130-166.airbears2.1918.berkeley.edu>
2018-11-28 20:36:27 -0800

    mapp

--- CryBears/CryBears/MapViewController.swift
+++ CryBears/CryBears/MapViewController.swift
@@ -7,24 +7,120 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import Firebase
 
 class MapViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 1000
+    
+    var ref: DatabaseReference?
+    var Handle: DatabaseHandle?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkLocationServices()
+        
+        ref = Database.database().reference()
+        
+        ref?.child("Posts").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            var array = [[String: Any?]]()
+            
+            guard let children = snapshot.children.allObjects as? [DataSnapshot]
+                else { return }
+            
+            for child in children {
+                guard let dict = child.value as? [String: Any] else { continue }
+                array.append(["content": dict["content"], "lat": dict["lat"], "lon": dict["lon"]])
+            }
+            
+            print(array)
+            
+            array.forEach { item in
+                print(item)
+                let userpin = MKPointAnnotation()
+                let coord = CLLocationCoordinate2DMake(item["lat"] as! CLLocationDegrees, item["lon"] as! CLLocationDegrees)
+                userpin.coordinate = coord
+                userpin.title = item["content"] as? String
+                self.map.addAnnotation(userpin)
+                
+            }
+        })
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //2
+    func setupLocationManager() {
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
-    */
+    
+    //4
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            map.setRegion(region, animated: true)
+            
+            
+        }
+    }
+    
+    //1
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+    }
+    
+    //3
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            map.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // Show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // Show an alert letting them know what's up
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+}
 
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        manager.startUpdatingLocation()
+        let myLocation = location.coordinate
+        
+        let region = MKCoordinateRegion(center: myLocation, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        map.setRegion(region, animated: true)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
 }
